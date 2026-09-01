b8c8028d3def17fad4f100e1cbc8a63c6451728e
Pascal Iske <info@pascaliske.dev>
2026-09-01 22:54:35 +0200

    chore(renovate): auto update default container images
    Signed-off-by: Pascal Iske <info@pascaliske.dev>

--- api/v1alpha1/mispinstance_funcs.go
+++ api/v1alpha1/mispinstance_funcs.go
@@ -23,9 +23,13 @@ import (
 	"strings"
 )
 
-// default container images
+// renovate: image=ghcr.io/misp/misp-docker/misp-core
 const mispInstanceCoreImageDefault = "ghcr.io/misp/misp-docker/misp-core:v2.5.44"
+
+// renovate: image=ghcr.io/misp/misp-docker/misp-nginx
 const mispInstanceNginxImageDefault = "ghcr.io/misp/misp-docker/misp-nginx:v2.5.44"
+
+// renovate: image=ghcr.io/misp/misp-docker/misp-modules
 const mispInstanceModulesImageDefault = "ghcr.io/misp/misp-docker/misp-modules:v3.0.9"
 
 // Returns the custom misp-core image if set or the default misp-core image as fallback
